561101f788b3241483b9c4d52a6acdaa8daa9a97
Michael Tambornino <michael.tambornino@gmail.com>
2016-12-28 12:09:56 -0600

    nailed it

--- converter.js
+++ converter.js
@@ -1,48 +1,58 @@
-// declare variables
+// collect and declare variables
 let tempIn = document.getElementById('tempIn');
 let tempOut = document.getElementById('tempOut');
 let button = document.getElementById('converter');
 let forFaren = document.getElementById('far');
 let forCels = document.getElementById('cel');
+let clear = document.getElementById('clear');
 
 
-function toCelsius (tempIn) {
-   tempOut.innerHTML = ((tempIn.value - 32) * (5 / 9))
-   // color the temp output, based on range
-   if (tempOut > 32) {
-      tempOut.className += " .red"
-   } else if (tempOut < 0) {
-      tempOut.className += " .blue"
+function toCelsius () {
+   x = Math.floor((tempIn.value - 32) * (5 / 9))
+   // color the temp output, based on temp range
+   if (x > 32) {
+      tempOut.className = "red"
+   } else if (x < 0) {
+      tempOut.className = "blue"
    } else {
-      tempout.className += " .green"
+      tempOut.className = "green"
    }
+   // output converted temp
+   tempOut.innerHTML = x + " C";
 }
 
-function toFahrenheit (tempIn) {
-   tempOut.innerHTML = (tempIn.value * (9 / 5) + 32)
-   // color the temp output, based on range
-   if (tempOut > 90) {
-      tempOut.className += " .red"
-   } else if (tempOut < 32) {
-      tempOut.className += " .blue"
+function toFahrenheit () {
+   x = Math.floor(tempIn.value * (9 / 5) + 32)
+   // color the temp output, based on temp range
+   if (x > 90) {
+      tempOut.className = "red"
+   } else if (x < 32) {
+      tempOut.className = "blue"
    } else {
-      tempout.className += " .green"
+      tempOut.className = "green"
    }
+   // output converted temp
+   tempOut.innerHTML = x + " F";
 }
-
 
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (e) {
-  console.log("event", e);
-  if ( ) {
-   toCelcius();
-  } else
-  if (  ) {
+  // console.log("event", e);
+  if (forCels.checked === true) {
+   toCelsius();
+  } else if (forFaren.checked === true) {
    toFahrenheit();
   }
 }
 
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
+
+
+// clear form when button is clicked
+clear.addEventListener('click', function() {
+   tempIn.value = "";
+   tempOut.innerHTML = "";
+})
